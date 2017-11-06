83216684288517e2c58e1f0fdabe99551dc78399
Ján Profant <jan.profant@phonexia.com>
2017-11-06 13:41:29 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='vbdiar',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='profant',
+    author_email='xprofa00@stud.fit.vutbr.cz',
+    description=''
+)
