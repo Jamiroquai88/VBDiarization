added score normalization, fixed some issues.

examples/diarization.py
@@ -2,25 +2,20 @@
 
 import argparse
 import ctypes
-import math
 import multiprocessing
 import os
 
 from scipy import signal
 from scipy.io.wavfile import read
 
+from vbdiar.features.raw2ivec import get_vad, get_segments, get_num_segments, RATE
+from vbdiar.scoring.normalization import Normalization
 from vbdiar.scoring.diarization import Diarization
 from vbdiar.scoring.plda import PLDA
 from vbdiar.utils.utils import loginfo, logwarning, Utils
 from vbdiar.features.features import Features
 from vbdiar.ivectors.fea2ivec import Fea2Ivec
 from vbdiar.ivectors.ivec import IvecSet
-from vbdiar.vad.vad import load_vad_lab_as_bool_vec
-
-RATE = 8000
-SOURCERATE = 1250
-TARGETRATE = 100000
-WINDOWSIZE = 250000.0
 
 
 def _process_files(dargs):
@@ -118,127 +113,6 @@ def process_file(wav_dir, vad_dir, out_dir, file_name, fea2ivec_obj, min_size, m
         return ivec_set
 
 
-def get_vad(file_name, fea_len):
-    """ Load .lab file as bool vector.
-
-    Args:
-        file_name (str): path to .lab file
-        fea_len (int): length of features
-
-    Returns:
-        np.array: bool vector
-    """
-
-    loginfo('Loading VAD from file {} ...'.format(file_name))
-    return load_vad_lab_as_bool_vec(file_name)[:fea_len]
-
-
-def get_segments(vad, min_size, max_size, tolerance):
-    """ Return clustered speech segments.
-
-        :param vad: list with labels - voice activity detection
-        :type vad: list
-        :param min_size: minimal size of window in ms
-        :type min_size: int
-        :param max_size: maximal size of window in ms
-        :type max_size: int
-        :param tolerance: accept given number of frames as speech even when it is marked as silence
-        :type tolerance: int
-        :returns: clustered segments
-        :rtype: list
-    """
-    clusters = get_clusters(vad, get_num_frames(min_size), tolerance)
-    segments = []
-    max_frames = get_num_frames(max_size)
-    for item in clusters.values():
-        if item[1] - item[0] > max_frames:
-            for ss in split_segment(item, max_frames):
-                segments.append(ss)
-        else:
-            segments.append(item)
-    return segments
-
-
-def split_segment(segment, max_size):
-    """ Split segment to more with adaptive size.
-
-        :param segment: input segment
-        :type segment: tuple
-        :param max_size: maximal size of window in ms
-        :type max_size: int
-        :returns: splitted segment
-        :rtype: list
-    """
-    size = segment[1] - segment[0]
-    num_segments = int(math.ceil(size / max_size))
-    size_segment = size / num_segments
-    for ii in range(num_segments):
-        yield (segment[0] + ii * size_segment, segment[0] + (ii + 1) * size_segment)
-
-
-def get_num_frames(n):
-    """ Get number of frames from ms.
-
-        :param n: number of ms
-        :type n: int
-        :returns: number of frames
-        :rtype: int
-
-        >>> get_num_frames(25)
-        1
-        >>> get_num_frames(35)
-        2
-    """
-    return int(1 + (n - WINDOWSIZE / 10000) / (TARGETRATE / 10000))
-
-
-def get_num_segments(n):
-    """ Get count of ms from number of frames.
-
-        :param n: number of frames
-        :type n: int
-        :returns: number of ms
-        :rtype: int
-
-        >>> get_num_segments(1)
-        25
-        >>> get_num_segments(2)
-        35
-
-    """
-    return int(n * (TARGETRATE / 10000) - (TARGETRATE / 10000) + (WINDOWSIZE / 10000))
-
-
-def get_clusters(vad, min_size, tolerance=10):
-    """ Cluster speech segments.
-
-        :param vad: list with labels - voice activity detection
-        :type vad: list
-        :param min_size: minimal size of window in ms
-        :type min_size: int
-        :param tolerance: accept given number of frames as speech even when it is marked as silence
-        :type tolerance: int
-        :returns: clustered speech segments
-        :rtype: dict
-    """
-    num_prev = 0
-    in_tolerance = 0
-    num_clusters = 0
-    clusters = {}
-    for ii, frame in enumerate(vad):
-        if frame:
-            num_prev += 1
-        else:
-            in_tolerance += 1
-            if in_tolerance > tolerance:
-                if num_prev > min_size:
-                    clusters[num_clusters] = (ii - num_prev, ii)
-                    num_clusters += 1
-                num_prev = 0
-                in_tolerance = 0
-    return clusters
-
-
 def set_mkl(num_cores=1):
     """ Set number of cores for mkl library.
 
@@ -246,30 +120,40 @@ def set_mkl(num_cores=1):
         :type num_cores: int
     """
     mkl_rt = ctypes.CDLL('libmkl_rt.so')
+    mkl_rt.mkl_set_dynamic(ctypes.byref(ctypes.c_int(0)))
     mkl_rt.mkl_set_num_threads(ctypes.byref(ctypes.c_int(num_cores)))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('Extract i-vectors used for diarization from audio wav files.')
+
     # required
     parser.add_argument('-l', '--input-list', help='list of input files without suffix',
                         action='store', dest='input_list', type=str, required=True)
     parser.add_argument('-c', '--configuration', help='input configuration of models',
                         action='store', dest='configuration', type=str, required=True)
-    parser.add_argument('--audio-dir', help='directory with audio files in .wav format - 8000Hz, 16bit-s, 1c',
-                        action='store', dest='audio_dir', type=str, required=True)
-    parser.add_argument('--rttm-dir', help='output directory for storing rttm files',
-                        action='store', dest='rttm_dir', type=str, required=True)
-    parser.add_argument('--vad-dir', help='directory with lab files - Voice/Speech activity detection',
-                        action='store', dest='vad_dir', type=str, required=True)
 
     # not required
-    parser.add_argument('--ivec-dir', help='output directory for storing i-vectors files',
-                        action='store', dest='ivec_dir', type=str, required=False)
+    parser.add_argument('--audio-dir', help='directory with audio files in .wav format - 8000Hz, 16bit-s, 1c',
+                        action='store', dest='audio_dir', type=str, required=False)
+    parser.add_argument('--vad-dir', help='directory with lab files - Voice/Speech activity detection',
+                        action='store', dest='vad_dir', type=str, required=False)
+    parser.add_argument('--in-ivec-dir', help='input directory containing i-vectors',
+                        action='store', dest='in_ivec_dir', type=str, required=False)
+    parser.add_argument('--out-ivec-dir', help='output directory for storing i-vectors',
+                        action='store', dest='out_ivec_dir', type=str, required=False)
+    parser.add_argument('--norm-list', help='list of normalization files without suffix',
+                        action='store', dest='norm_list', type=str, required=False)
+    parser.add_argument('--in-rttm-dir', help='input directory with rttm files',
+                        action='store', dest='in_rttm_dir', type=str, required=False)
+    parser.add_argument('--out-rttm-dir', help='output directory for storing rttm files',
+                        action='store', dest='out_rttm_dir', type=str, required=False)
     parser.add_argument('-wav-suffix', help='wav file suffix',
                         action='store', dest='wav_suffix', type=str, required=False)
     parser.add_argument('-vad-suffix', help='Voice Activity Detector file suffix',
                         action='store', dest='vad_suffix', type=str, required=False)
+    parser.add_argument('-rttm-suffix', help='rttm file suffix',
+                        action='store', dest='rttm_suffix', type=str, required=False)
     parser.add_argument('--min-window-size', help='minimal window size for extracting i-vector in ms',
                         action='store', dest='min_window_size', type=int, required=False)
     parser.add_argument('--max-window-size', help='maximal window size for extracting i-vector in ms',
@@ -278,13 +162,16 @@ if __name__ == '__main__':
                         action='store', dest='vad_tolerance', type=int, required=False)
     parser.add_argument('-j', '--num-threads', help='number of processor threads to use',
                         action='store', dest='num_threads', type=int, required=False)
+    parser.add_argument('--max-num-speakers', help='maximal number of speakers',
+                        action='store', dest='max_num_speakers', type=int, required=False)
     parser.set_defaults(num_cores=1)
+    parser.set_defaults(max_num_speakers=10)
     parser.set_defaults(wav_suffix='wav')
     parser.set_defaults(vad_suffix='lab.gz')
+    parser.set_defaults(rttm_suffix='rttm')
     parser.set_defaults(min_window_size=1000)
-    parser.set_defaults(max_window_size=2000)
-    parser.set_defaults(vad_tolerance=5)
-    parser.set_defaults(ivec_dir=None)
+    parser.set_defaults(max_window_size=4000)
+    parser.set_defaults(vad_tolerance=2)
     args = parser.parse_args()
 
     set_mkl(1)
@@ -295,23 +182,32 @@ if __name__ == '__main__':
     files = [line.rstrip('\n') for line in open(args.input_list)]
 
     # extract i-vectors
-    if args.ivec_dir is None:
+    if args.in_ivec_dir is None:
         ivec = process_files(
-            files, args.audio_dir, args.vad_dir, args.ivec_dir, fea2ivec, args.min_window_size,
+            files, args.audio_dir, args.vad_dir, args.in_ivec_dir, fea2ivec, args.min_window_size,
             args.max_window_size, args.vad_tolerance, args.wav_suffix, args.vad_suffix, args.num_threads)
+        if args.out_ivec_dir:
+            for ivecset in ivec:
+                ivecset.save('{}.{}'.format(os.path.join(args.out_ivec_dir, ivecset.name), 'pkl'))
     else:
-        ivec = args.ivec_dir
+        ivec = args.in_ivec_dir
 
-    # initialize diarization
-    norm = None
+    # initialize PLDA model
     try:
         plda = PLDA(config['PLDA']['model_path'])
     except IOError:
         logwarning('PLDA model initialization failed. Cosine distance will be used instead.')
         plda = None
 
+    # initialize normalization
+    if args.norm_list is not None:
+        norm = Normalization(args.norm_list, args.audio_dir, args.in_rttm_dir, args.in_ivec_dir, args.out_ivec_dir,
+                             fea2ivec, plda, args.wav_suffix, args.rttm_suffix)
+    else:
+        norm = None
+
     # run diarization
     diar = Diarization(args.input_list, ivec, norm, plda)
-    scores = diar.score_ivec()
-    if args.rttm_dir is not None:
-        diar.dump_rttm(scores, args.rttm_dir)
+    scores = diar.score_ivec(args.max_num_speakers)
+    if args.out_rttm_dir is not None:
+        diar.dump_rttm(scores, args.out_rttm_dir)

vbdiar/ivectors/gmm.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2017 Phonexia
+# Copyright (C) 2017 Brno University of Technology FIT
 # Author: Jan Profant <jan.profant@phonexia.com>
 # All Rights Reserved
+
 import h5py
 import scipy
-import struct
 import scipy.linalg
 import numpy as np
 

vbdiar/scoring/diarization.py
@@ -5,6 +5,7 @@ import re
 import pickle
 
 import numpy as np
+from pyclustering.cluster.xmeans import xmeans
 from sklearn.cluster import KMeans as sklearnKMeans
 from sklearn.metrics.pairwise import cosine_similarity
 
@@ -82,7 +83,7 @@ class Diarization(object):
                     logwarning(
                         '[Diarization.load_ivecs] No pickle file found for {}.'.format(line.rstrip().split()[0]))
 
-    def score_ivec(self):
+    def score_ivec(self, max_num_speakers):
         """ Score i-vectors agains speaker clusters.
 
             :returns: PLDA scores
@@ -103,8 +104,9 @@ class Diarization(object):
                     else:
                         centroids = PLDAKMeans(sklearnkmeans.cluster_centers_, num_speakers, self.plda).fit(ivecs)
                 else:
-                    # TODO estimate number of speakers
-                    raise NotImplementedError
+                    xm = xmeans(ivecs, kmax=max_num_speakers)
+                    xm.process()
+                    centroids = np.array(xm.get_clusters())
                 if self.norm is None:
                     if self.plda is None:
                         scores_dict[name] = cosine_similarity(ivecs, centroids).T

vbdiar/scoring/normalization.py
@@ -1,59 +1,120 @@
 #!/usr/bin/env python
 
 import os
-import pickle
 
+import h5py
 import numpy as np
+from scipy import signal
+from scipy.io.wavfile import read
+from sklearn.metrics.pairwise import cosine_similarity
 
+from vbdiar.features.features import Features
+from vbdiar.features.raw2ivec import RATE, get_num_frames
 from vbdiar.scoring.plda import PLDA
-from vbdiar.utils.utils import loginfo, logwarning
+from vbdiar.utils.utils import loginfo, logwarning, Utils
 
 
 class Normalization(object):
-    """ Speaker normalization S-Norm. Handles also some other operation as calibration, detecting number of speakers.
+    """ Speaker normalization S-Norm.
 
     """
-    def __init__(self, ivecs_dir, norm_list, plda_model_dir):
-        """ Class constructor.
+    norm_ivec = None
 
-            :param ivecs_dir: path to directory with i-vectors
-            :type ivecs_dir: str
-            :param norm_list: path to list with files relative to directory with i-vectors
-            :type norm_list: str
-            :param plda_model_dir: path to directory with models
-            :type plda_model_dir: str
+    def __init__(self, norm_list, audio_dir=None, rttm_dir=None, in_ivec_dir=None, out_ivec_dir=None,
+                 fea2ivec=None, plda=None, audio_suffix='wav', rttm_suffix='rttm'):
         """
-        self.ivecs_dir = ivecs_dir
-        self.scale, self.shift, self.model = None, None, None
-        self.norm_list = norm_list
-        self.plda = PLDA(plda_model_dir)
-        if self.norm_list is not None:
-            self.norm_ivecs = np.array(list(self.load_norm_ivecs()))
-        else:
-            self.norm_ivecs = None
 
-    def load_norm_ivecs(self):
+        Args:
+            norm_list (str): path to normalization list
+            audio_dir (str|None): path to audio directory
+            rttm_dir (str|None): path to directory with rttm files
+            in_ivec_dir (str|None): path to directory with i-vectors
+            out_ivec_dir (str|None): path to directory for storing i-vectors
+            fea2ivec (Fea2Ivec|None): initialized Fea2Ivec object
+            plda (PLDA|None): plda model object
+            audio_suffix (str): suffix of wav files
+            rttm_suffix (str): suffix of rttm files
+        """
+        self.audio_dir = audio_dir
+        self.norm_list = norm_list
+        self.rttm_dir = rttm_dir
+        self.fea2ivec = fea2ivec
+        self.plda = plda
+        self.audio_suffix = audio_suffix
+        self.rttm_suffix = rttm_suffix
+        self.in_ivec_dir = in_ivec_dir
+        self.out_ivec_dir = out_ivec_dir
+        if self.in_ivec_dir is None:
+            self.norm_ivec = self.extract_ivecs()
+        else:
+            self.norm_ivec = self.load_ivecs()
+
+    def extract_ivecs(self):
+        """ Extract normalization i-vectors using averaging.
+
+            Returns:
+
+        """
+        speakers_dict = {}
+
+        with open(self.norm_list, 'r') as f:
+            for line in f:
+                if len(line.split()) > 1:  # number of speakers is defined
+                    line = line.split()[0]
+                speakers_dict = self.process_file(line, speakers_dict)
+
+        for dict_key in speakers_dict:
+            speakers_dict[dict_key] = np.mean(speakers_dict[dict_key], axis=0)
+
+        if self.out_ivec_dir:
+            for speaker in speakers_dict:
+                Utils.mkdir_p(os.path.join(self.out_ivec_dir, os.path.dirname(speaker)))
+                h5file = h5py.File('{}.{}'.format(os.path.join(self.out_ivec_dir, speaker), 'h5'), 'w')
+                h5file.create_dataset(speaker, data=speakers_dict[speaker])
+                h5file.close()
+        return np.array(speakers_dict.values())
+
+    def process_file(self, file_name, speakers_dict):
+        loginfo('Processing file {} ...'.format(file_name.split()[0]))
+        wav = '{}.{}'.format(os.path.join(self.audio_dir, file_name), self.audio_suffix)
+        rate, sig = read(wav)
+        if len(sig.shape) != 1:
+            raise ValueError('Expected mono as input audio.')
+        if rate != RATE:
+            logwarning('The input file is expected to be in 8000 Hz, got {} Hz instead, resampling.'.format(rate))
+            sig = signal.resample(sig, RATE)
+
+        fea_extractor = Features()
+        fea = fea_extractor(sig)
+
+        with open('{}.{}'.format(os.path.join(self.rttm_dir, file_name), self.rttm_suffix), 'r') as f:
+            for line in f:
+                start, dur, speaker = float(line.split()[3]) * 1000, float(line.split()[4]) * 1000, line.split()[7]
+                end = start + dur
+                start, end = get_num_frames(start), get_num_frames(end)
+                w = self.fea2ivec.get_ivec(fea[start:end])
+                if speaker not in speakers_dict.keys():
+                    speakers_dict[speaker] = w
+                else:
+                    speakers_dict[speaker] = np.append(speakers_dict[speaker], w, axis=0)
+        return speakers_dict
+
+    def load_ivecs(self):
         """ Load normalization i-vectors, scale and shift files and also pretrained model.
 
             :returns: i-vectors
             :rtype: numpy.array
         """
-        line = None
+        ivecs_list = []
         with open(self.norm_list, 'r') as f:
             for line in f:
-                line = line.rstrip()
-                loginfo('[Diarization.load_norm_ivecs] Loading npy file {} ...'.format(line))
-                try:
-                    yield np.load('{}.npy'.format(os.path.join(self.ivecs_dir, line))).flatten()
-                except IOError:
-                    logwarning('[Diarization.load_norm_ivecs] No pickle file found for {}.'.format(line))
-        self.scale = np.load(os.path.join(self.ivecs_dir, os.path.dirname(line), 'scale.npy'))
-        self.shift = np.load(os.path.join(self.ivecs_dir, os.path.dirname(line), 'shift.npy'))
-        try:
-            with open(os.path.join(self.ivecs_dir, os.path.dirname(line), 'model.pkl')) as f:
-                self.model = pickle.load(f)
-        except IOError:
-            logwarning('[Diarization.load_norm_ivecs] No pretrained model found.')
+                if len(line.split()) > 1:
+                    line = line.split()[0]
+                    loginfo('Loading h5 normalization file {} ...'.format(line))
+                    h5file = h5py.File('{}.{}'.format(os.path.join(self.in_ivec_dir, line), 'h5'), 'r')
+                    for h5_key in h5file.keys():
+                        ivecs_list.append(h5file[h5_key][:].flatten())
+        return np.array(ivecs_list)
 
     def s_norm(self, test, enroll):
         """ Run S-Norm on input i-vectors.
@@ -65,9 +126,14 @@ class Normalization(object):
             :returns: scores matrix
             :rtype: numpy.array
         """
-        a = self.plda.score(test, self.norm_ivecs, scale=self.scale, shift=self.shift)
-        b = self.plda.score(enroll, self.norm_ivecs, scale=self.scale, shift=self.shift)
-        c = self.plda.score(enroll, test, scale=self.scale, shift=self.shift)
+        if self.plda:
+            a = self.plda.score(test, self.norm_ivec)
+            b = self.plda.score(enroll, self.norm_ivec)
+            c = self.plda.score(enroll, test)
+        else:
+            a = cosine_similarity(test, self.norm_ivec).T
+            b = cosine_similarity(enroll, self.norm_ivec).T
+            c = cosine_similarity(enroll, test).T
         scores = []
         for ii in range(test.shape[0]):
             test_scores = []
@@ -78,14 +144,3 @@ class Normalization(object):
                 test_scores.append((((s - test_mean) / test_std + (s - enroll_mean) / enroll_std) / 2))
             scores.append(test_scores)
         return np.array(scores).T
-
-    @staticmethod
-    def get_features(scores):
-        """ Compute features from input scores.
-
-            :param scores: input scores
-            :type scores: list
-            :returns: mean, std and median
-            :rtype: tuple
-        """
-        return np.mean(scores), np.std(scores), np.median(scores)